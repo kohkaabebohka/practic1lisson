e99626c01b17f158b7b6f5c6a4d48f476c904617
Николай <nikolajorlov043@gmail.com>
2022-12-22 11:28:27 +0300

    DZ

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите переменную а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите переменную b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
